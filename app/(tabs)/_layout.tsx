93292482e50c76cd1a554463e0d5780196a8e395
Fizzah Sikander <123269709+FizzahSikander@users.noreply.github.com>
2025-08-21 16:23:42 +0200

    camera feature added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'camera',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
